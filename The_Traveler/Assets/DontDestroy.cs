4e8fd4ac3239f1666a3169d19e6234097c77443e
Alexis <aurqui7@uic.edu>
2019-03-19 02:45:02 -0500

    implemented time travel mechanic

---
+++ The_Traveler/Assets/DontDestroy.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroy : MonoBehaviour
+{
+    void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("Player");
+        GameObject[] scenes = GameObject.FindGameObjectsWithTag("SceneManager");
+        if (objs.Length > 1)
+            Destroy(objs[1]);
+        if (scenes.Length > 1)
+            Destroy(scenes[1]);
+
+        DontDestroyOnLoad(objs[0]);
+        //DontDestroyOnLoad(GameObject.FindWithTag("MainCamera"));
+    }
+}
---
+++ The_Traveler/Assets/TimeTravelManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TimeTravelManager : MonoBehaviour
+{
+    private Scene currentScene;
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (Input.GetKey(KeyCode.F))
+        //    SingletonDataHolder.setData(SingletonDataHolder.getData() + 1);
+        //Debug.Log(SingletonDataHolder.getData());
+        currentScene = SceneManager.GetActiveScene();
+        if(Input.GetKey(KeyCode.Q))
+        {
+            SingletonDataHolder.SetPlayerPosition(player.transform.position);
+            if(currentScene.name.Equals("Level_01_Good_Timeline"))
+                SceneManager.LoadScene("SampleScene");
+            else
+                SceneManager.LoadScene("Level_01_Good_Timeline");
+            //Application.LoadLevel("Level_01_Good_Timeline");
+        }
+    }
+
+    void OnSceneLoaded()
+    {
+        player.transform.position = SingletonDataHolder.GetPlayerPosition();
+    }
+}
