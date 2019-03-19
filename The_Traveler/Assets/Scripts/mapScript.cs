40353c39b433de079e8ea8d25c5e515ca76a7c6d
larryngo97 <larryngo97@gmail.com>
2019-03-19 13:06:10 -0500

    Create mapScript.cs
    map script

---
+++ The_Traveler/Assets/Scripts/mapScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mapScript : MonoBehaviour
+{
+    public Transform target;
+
+    void LateUpdate()
+    {
+        Vector3 targetPosition = target.position;
+        targetPosition.y = transform.position.y;
+        transform.position = targetPosition;
+    }
+}
