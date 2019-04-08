b376852588b3461f709aa8ad1cb877eb90b116b2
Alexis <aurqui7@uic.edu>
2019-04-08 18:05:15 -0500

    fixed time travel bug and integrated travel with UI

--- The_Traveler/Assets/Scripts/TimeTravelManager.cs
+++ The_Traveler/Assets/Scripts/TimeTravelManager.cs
@@ -2,39 +2,75 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class TimeTravelManager : MonoBehaviour
 {
-    public GameObject player;
+    public Transform player;
+    public CharacterController c;
     public bool traveled = false;
     public float travelCooldown = 0f;
-    private Vector3 offsetTravel = new Vector3(0, 0, 1000);
+    public Vector3 offsetTravel = new Vector3(0, 0, 1000);
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        travelCooldown -= Time.deltaTime;
-        //Debug.Log(travelCooldown);
+        //travelCooldown -= Time.deltaTime;
 
-        if(Input.GetKeyDown(KeyCode.Q) && traveled == false && travelCooldown < 0)
-        {
-            Debug.Log("travled forward");
-            Debug.Log(traveled);
-            Debug.Log(travelCooldown);
-            travelCooldown = 5f;
-            player.transform.position += offsetTravel;
-            traveled = true;
+        //if (Input.GetKeyDown(KeyCode.Q) && traveled == false && travelCooldown < 0)
+        //{
+        //    Debug.Log("travled forward");
+        //    Debug.Log(traveled);
+        //    Debug.Log(travelCooldown);
 
-        } else if (Input.GetKeyDown(KeyCode.Q) && traveled == true && travelCooldown < 0)
-        {
-            Debug.Log("Traveled backward");
-            Debug.Log(traveled);
-            Debug.Log(travelCooldown);
-            travelCooldown = 5f;
-            player.transform.position -= offsetTravel;
-            traveled = false;
-        }
+        //    travelCooldown = 5f;
+        //    //c.Move(offsetTravel + transform.position);
+        //    c.enabled = false;
+        //    transform.position += offsetTravel;
+        //    c.enabled = true;
+        //    traveled = true;
 
-        
+        //}
+        //else if (Input.GetKeyDown(KeyCode.Q) && traveled == true && travelCooldown < 0)
+        //{
+
+        //    Debug.Log("Traveled backward");
+        //    Debug.Log(traveled);
+        //    Debug.Log(travelCooldown);
+
+        //    travelCooldown = 5f;
+        //    c.enabled = false;
+        //    transform.position -= offsetTravel;
+        //    c.enabled = true;
+        //    traveled = false;
+        //}
+
+
+    }
+
+    public bool travelForward()
+    {
+        Debug.Log("travled forward");
+        Debug.Log(traveled);
+        Debug.Log(travelCooldown);
+
+        travelCooldown = 5f;
+        c.enabled = false;
+        transform.position += offsetTravel;
+        c.enabled = true;
+        return true;
+    }
+
+    public bool travelBackward()
+    {
+        Debug.Log("Traveled backward");
+        Debug.Log(traveled);
+        Debug.Log(travelCooldown);
+
+        travelCooldown = 5f;
+        c.enabled = false;
+        transform.position -= offsetTravel;
+        c.enabled = true;
+        return false;
     }
 }
--- The_Traveler/Assets/Scripts/TravelBar.cs
+++ The_Traveler/Assets/Scripts/TravelBar.cs
@@ -9,6 +9,9 @@ public class TravelBar : MonoBehaviour
     public Slider travelBar;
     public Image travelFill;
     private float timeToTravel = 30f;
+    public bool traveled = false;
+    public TimeTravelManager manager;
+    public float cooldown = 0f;
 
     // Start is called before the first frame update
     void Start()
@@ -18,12 +21,37 @@ public class TravelBar : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-        travelBar.value = Time.time / timeToTravel;
+    {   
+        // add to the cooldown
+        cooldown += Time.deltaTime;
+
+        // calculate to let player know through UI
+        travelBar.value = cooldown / timeToTravel;
         travelFill.color = Color.Lerp(Color.gray, Color.cyan, travelBar.value);
+
+        // once the ability is up the player can travel
         if(travelBar.value == 1) //maxed out
         {
-            Debug.Log("TIME TRAVEL READY");
+            // travel depending on what timeline
+            if (Input.GetKeyDown(KeyCode.Q) && traveled == false)
+            {
+                traveled = manager.travelForward();
+
+                // reset cooldown bar
+                cooldown = 0;
+                travelBar.value = cooldown / timeToTravel;
+                travelFill.color = Color.Lerp(Color.gray, Color.cyan, travelBar.value);
+            }
+            else if (Input.GetKeyDown(KeyCode.Q) && traveled == true)
+            {
+                traveled = manager.travelBackward();
+
+                // reset cooldown bar
+                cooldown = 0;
+                travelBar.value = cooldown / timeToTravel;
+                travelFill.color = Color.Lerp(Color.gray, Color.cyan, travelBar.value);
+            }
+
         }
         /* IF PLAYER USES TIME TRAVEL 
          * RESET TRAVEL BAR TO 0
