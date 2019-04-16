719cbae263d29ea4e9b7bd858ceacb93275c0f4f
aarias9 <aarias9@uic.edu>
2019-04-16 01:06:01 -0500

    Adding lights/Textures
    In Good timezone area, also added a script to flicker the lights.

---
+++ The_Traveler/Assets/Scripts/Flickering_Lights.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flickering_Lights : MonoBehaviour
+{
+    Light testLight;
+    public float minWaitTime;
+    public float maxWaitTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        testLight = GetComponent<Light>();
+        StartCoroutine(Flashing());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator Flashing()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(Random.Range(minWaitTime, maxWaitTime));
+            testLight.enabled = !testLight.enabled;
+        }
+    }
+}
