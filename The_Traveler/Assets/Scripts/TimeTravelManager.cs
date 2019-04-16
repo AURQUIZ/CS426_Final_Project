ab9a5ce0db10dd004afc8a0ac35891984df70593
Alexis <aurqui7@uic.edu>
2019-04-16 16:16:54 -0500

    added more background music

--- The_Traveler/Assets/Scripts/TimeTravelManager.cs
+++ The_Traveler/Assets/Scripts/TimeTravelManager.cs
@@ -11,48 +11,17 @@ public class TimeTravelManager : MonoBehaviour
     public bool traveled = false;
     public float travelCooldown = 0f;
     public Vector3 offsetTravel = new Vector3(0, 0, 1000);
-
-    // Update is called once per frame
-    void LateUpdate()
-    {
-        //travelCooldown -= Time.deltaTime;
-
-        //if (Input.GetKeyDown(KeyCode.Q) && traveled == false && travelCooldown < 0)
-        //{
-        //    Debug.Log("travled forward");
-        //    Debug.Log(traveled);
-        //    Debug.Log(travelCooldown);
-
-        //    travelCooldown = 5f;
-        //    //c.Move(offsetTravel + transform.position);
-        //    c.enabled = false;
-        //    transform.position += offsetTravel;
-        //    c.enabled = true;
-        //    traveled = true;
-
-        //}
-        //else if (Input.GetKeyDown(KeyCode.Q) && traveled == true && travelCooldown < 0)
-        //{
-
-        //    Debug.Log("Traveled backward");
-        //    Debug.Log(traveled);
-        //    Debug.Log(travelCooldown);
-
-        //    travelCooldown = 5f;
-        //    c.enabled = false;
-        //    transform.position -= offsetTravel;
-        //    c.enabled = true;
-        //    traveled = false;
-        //}
-
-
-    }
+    public AudioSource bad_music;
+    public AudioSource good_music;
 
     public bool travelForward()
     {
         Debug.Log("travled forward");
         Debug.Log(traveled);
         Debug.Log(travelCooldown);
+
+        bad_music.Stop();
+        good_music.Play();
 
         travelCooldown = 5f;
         c.enabled = false;
@@ -67,10 +36,17 @@ public class TimeTravelManager : MonoBehaviour
         Debug.Log(traveled);
         Debug.Log(travelCooldown);
 
+        bad_music.Play();
+        good_music.Stop();
         travelCooldown = 5f;
         c.enabled = false;
         transform.position -= offsetTravel;
         c.enabled = true;
         return false;
+    }
+
+    public void Awake()
+    {
+        bad_music.Play();
     }
 }
