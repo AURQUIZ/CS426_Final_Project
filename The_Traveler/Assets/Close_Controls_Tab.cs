366e0939ee06acc59eee9087dd8c59eb24124713
Alexis <aurqui7@uic.edu>
2019-04-23 23:24:48 -0500

    added controls tab

---
+++ The_Traveler/Assets/Close_Controls_Tab.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Close_Controls_Tab : MonoBehaviour
+{
+    public UI_Button_Handlers uiHandler;
+
+    public void back()
+    {
+        uiHandler.BackToPause();
+    }
+}
