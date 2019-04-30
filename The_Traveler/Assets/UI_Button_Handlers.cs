1a4f7adbab10a730a62d988b200de9a44efcf011
larryngo97 <larryngo97@gmail.com>
2019-04-29 22:06:11 -0500

    design

--- The_Traveler/Assets/UI_Button_Handlers.cs
+++ The_Traveler/Assets/UI_Button_Handlers.cs
@@ -6,10 +6,23 @@ using UnityEngine.UI;
 
 public class UI_Button_Handlers : MonoBehaviour
 {
+    public GameObject controlsText;
+    //public GameObject backButton;
+
+    public GameObject header;
+    public GameObject resume;
+    public GameObject controls;
+    public GameObject quit;
+
     public void Awake()
     {
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
+
+        header = GameObject.Find("Header");
+        resume = GameObject.Find("Resume");
+        controls = GameObject.Find("Controls");
+        quit = GameObject.Find("Quit");
     }
 
     public void BeginNewGame()
@@ -20,6 +33,40 @@ public class UI_Button_Handlers : MonoBehaviour
     public void BeginNewGameWithStory()
     {
         SceneManager.LoadScene("Introduction");
+    }
+
+    public void ShowCredits()
+    {
+        SceneManager.LoadScene("End_Game_Screen");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void ShowControlScheme()
+    {
+
+        header.SetActive(false);
+        resume.SetActive(false);
+        controls.SetActive(false);
+        quit.SetActive(false);
+
+        controlsText.SetActive(true);
+        //backButton.SetActive(true);
+    }
+
+    public void BackToPause()
+    {
+
+        header.SetActive(true);
+        resume.SetActive(true);
+        controls.SetActive(true);
+        quit.SetActive(true);
+
+        controlsText.SetActive(false);
+        //backButton.SetActive(false);
     }
 
     public void MainMenu()
